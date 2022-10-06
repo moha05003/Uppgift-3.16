b103991b5c7a1e9f67edaa4a70e43abb127302f4
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-10-06 13:57:30 +0200

    Add project files.

---
+++ Uppgift 3.16/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Uppgift_3_16
+{
+
+}
+
+ class Program
+{
+	 static void Main()
+	{
+
+		Console.WriteLine("Hur lång är din låt? ");
+		int time = int.Parse(Console.ReadLine());
+		int minimum = int.Parse(Console.ReadLine());
+		int total = (time * 60) + minimum;
+		int x = 260;
+		int y = 165;
+
+		
+		if (total <= x && total >= y)                                                                                                                                   
+		{
+			Console.WriteLine(" Din låt får vara med Radion! ");
+		}
+
+
+		else { Console.WriteLine("Tyvär så kan vi inte lägga din låt i Radion!"); }
+
+
+
+
+
+
+
+	}
+}
